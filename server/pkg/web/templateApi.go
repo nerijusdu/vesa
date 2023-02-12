d72a4b3adafb3488584b06533b99a9c77ffe6484
Nerijus <dnerijus72@gmail.com>
2023-02-12 15:33:19 +0200

    add update endpoint for a template

--- server/pkg/web/templateApi.go
+++ server/pkg/web/templateApi.go
@@ -2,7 +2,10 @@ package web
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"net/url"
+	"strings"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/nerijusdu/vesa/pkg/data"
@@ -86,12 +89,7 @@ func (api *VesaApi) registerTemplateRoutes(router chi.Router) {
 			return
 		}
 
-		if template.Container.Labels == nil {
-			template.Container.Labels = make(map[string]string)
-		}
-		template.Container.Labels["template"] = template.ID
-
-		id, err := api.dockerctrl.RunContainer(template.Container)
+		id, err := useTemplate(api, template)
 		if err != nil {
 			handleError(w, err)
 			return
@@ -100,4 +98,73 @@ func (api *VesaApi) registerTemplateRoutes(router chi.Router) {
 		w.WriteHeader(http.StatusCreated)
 		sendJson(w, &CreatedResponse{Id: id})
 	})
+
+	router.Post("/templates/{id}/update", func(w http.ResponseWriter, r *http.Request) {
+		template, err := api.templates.GetTemplate(chi.URLParam(r, "id"))
+		if err != nil {
+			handleError(w, err)
+			return
+		}
+
+		tag, err := url.QueryUnescape(r.URL.Query().Get("tag"))
+		if err != nil || tag == "" {
+			fmt.Println("Tag cound not be identified, using latest")
+			tag = "latest"
+		}
+
+		splits := strings.Split(template.Container.Image, ":")
+		if len(splits) > 1 {
+			splits[len(splits)-1] = tag
+		}
+
+		template.Container.Image = strings.Join(splits, ":")
+		if !template.Container.IsLocal {
+			fmt.Println("Skiping pull for local image")
+			err = api.dockerctrl.PullImage(template.Container.Image)
+			if err != nil {
+				handleError(w, err)
+				return
+			}
+		}
+
+		containers, err := api.dockerctrl.GetContainers(dockerctrl.GetContainersRequest{
+			Label: "template=" + template.ID,
+		})
+		if err != nil {
+			handleError(w, err)
+			return
+		}
+
+		if len(containers) > 0 {
+			c := containers[0]
+			err = api.dockerctrl.StopContainer(c.ID)
+			if err != nil {
+				handleError(w, err)
+				return
+			}
+
+			err = api.dockerctrl.DeleteContainer(c.ID)
+			if err != nil {
+				handleError(w, err)
+				return
+			}
+		}
+
+		_, err = useTemplate(api, template)
+		if err != nil {
+			handleError(w, err)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+	})
+}
+
+func useTemplate(api *VesaApi, template data.Template) (string, error) {
+	if template.Container.Labels == nil {
+		template.Container.Labels = make(map[string]string)
+	}
+	template.Container.Labels["template"] = template.ID
+
+	return api.dockerctrl.RunContainer(template.Container)
 }
