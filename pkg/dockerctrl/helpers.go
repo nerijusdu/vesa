0562e9118840ee97c5ae1be243f7ee4328d77cf7
Nerijus <dnerijus72@gmail.com>
2024-10-15 23:06:49 +0300

    fix https redirects

--- pkg/dockerctrl/helpers.go
+++ pkg/dockerctrl/helpers.go
@@ -23,12 +23,16 @@ func addDomainLabels(req RunContainerRequest) map[string]string {
 		req.Labels["traefik.enable"] = "true"
 		req.Labels["traefik.http.routers."+req.Name+".rule"] = "Host(`" + req.Domain.Host + "`)"
 		for _, e := range req.Domain.Entrypoints {
-			req.Labels["traefik.http.routers."+req.Name+".entrypoints"] = e
 			if strings.Contains(e, "websecure") {
 				req.Labels["traefik.http.routers."+req.Name+".tls.certResolver"] = "vesaresolver"
-				if e == "websecure" {
-					req.Labels["traefik.http.routers."+req.Name+".entrypoints.websecure.http.redirections.entryPoint.to"] = "websecure"
-				}
+			}
+			if e == "websecure" {
+				req.Labels["traefik.http.middlewares.redirect-to-https.redirectscheme.scheme"] = "https"
+				req.Labels["traefik.http.middlewares.redirect-to-https.redirectscheme.permanent"] = "true"
+				req.Labels["traefik.http.routers."+req.Name+".middlewares"] = "redirect-to-https"
+				req.Labels["traefik.http.routers."+req.Name+".entrypoints"] = "web,websecure"
+			} else {
+				req.Labels["traefik.http.routers."+req.Name+".entrypoints"] = e
 			}
 		}
 	}
