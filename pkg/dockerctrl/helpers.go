e932cd3fbce3bb34b42a665de4c177c846dc293b
Nerijus <dnerijus72@gmail.com>
2024-10-15 22:13:53 +0300

    fix resolver

--- pkg/dockerctrl/helpers.go
+++ pkg/dockerctrl/helpers.go
@@ -2,6 +2,7 @@ package dockerctrl
 
 import (
 	"os"
+	"strings"
 
 	"github.com/docker/docker/api/types/mount"
 )
@@ -23,7 +24,7 @@ func addDomainLabels(req RunContainerRequest) map[string]string {
 		req.Labels["traefik.http.routers."+req.Name+".rule"] = "Host(`" + req.Domain.Host + "`)"
 		for _, e := range req.Domain.Entrypoints {
 			req.Labels["traefik.http.routers."+req.Name+".entrypoints"] = e
-			if e == "websecure" {
+			if strings.Contains(e, "websecure") {
 				req.Labels["traefik.http.routers."+req.Name+".tls.certResolver"] = "vesaresolver"
 			}
 		}
